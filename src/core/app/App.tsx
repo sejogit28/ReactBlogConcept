e4a55c31a9218e886ede6d17013a96b121ca6d02
Sean Joseph <69924413+sejogit28@users.noreply.github.com>
2021-12-05 15:47:37 -0600

    Implemented dark mode using Theme Provider

--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {useState } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import {Grid, Paper} from '@mui/material';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
 
 import MuiAppBar from '../../nav/MuiAppBar';
 
-import {Grid} from '@mui/material';
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+  const muiTheme = createTheme(
+  {
+    palette: 
+    {
+      mode: darkMode ? 'dark' : 'light'
+    }
+  })
+
   return (
     <Router>
-      <MuiAppBar>
-        <Grid>
-          <div className="App">
-            <header className="App-header">
-              <p>
-                Design Time!!!
-              </p>
-            </header>
-          </div>
-        </Grid>
-      </MuiAppBar>
+      <ThemeProvider theme={muiTheme}>
+        <MuiAppBar setDarkMode={setDarkMode} darkMode={darkMode}>
+            <Paper style={{height: "100vh"}}>
+              <Grid>
+                <div className="App">
+                  <header className="App-header">
+                    <p>
+                      Design Time!!!
+                    </p>
+                  </header>
+                </div>
+              </Grid>
+            </Paper>
+        </MuiAppBar>
+      </ThemeProvider>
     </Router>
   );
 }
--- src/nav/MuiAppBar.tsx
+++ src/nav/MuiAppBar.tsx
@@ -1,14 +1,19 @@
 import React, { ReactElement } from 'react'
-import {AppBar, Toolbar, Typography, CssBaseline, useScrollTrigger, Slide} from '@mui/material';
+import {AppBar, Toolbar, Typography, CssBaseline, useScrollTrigger, Slide, IconButton} from '@mui/material';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+ import Brightness6Icon from '@mui/icons-material/Brightness6';
 
-interface AppBarProps {
+interface AppBarProps 
+{
     children: React.ReactElement;
+    setDarkMode:  React.Dispatch<React.SetStateAction<boolean>>;
+    darkMode: boolean;
 }
 
 
-function HideOnScroll(props: AppBarProps) {
-  const { children } = props;
+function HideOnScroll(appBarProps: AppBarProps) 
+{
+  const { children } = appBarProps;
   const trigger = useScrollTrigger();
   return (
     <Slide appear={false} direction="down" in={!trigger}>
@@ -17,19 +22,26 @@ function HideOnScroll(props: AppBarProps) {
   );
 }
 
-function MuiAppBar(appBarProps: AppBarProps): ReactElement {
+function MuiAppBar(appBarProps: AppBarProps): ReactElement 
+{
+  const {setDarkMode, darkMode} = appBarProps;
+
     return (
-<React.Fragment>
+    <React.Fragment>
       <CssBaseline />
-      <HideOnScroll {...appBarProps}>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6" component="div">
-              Best Blog Bro
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
+        <HideOnScroll {...appBarProps}>
+          <AppBar>
+            <Toolbar>
+              <DeveloperModeIcon/>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Best Blog Bro
+              </Typography>
+              <IconButton onClick={() =>{ setDarkMode(!darkMode)}}>
+                <Brightness6Icon/>
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
       <Toolbar />
     </React.Fragment>
     )
